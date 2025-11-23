05cfdf79c19bcf7a90630d58fcb6c95186d29979
jsprolsson <jesper.p.olsson@hotmail.com>
2025-11-24 00:27:19 +0100

    install pick and update lsps

--- base/nvim/init.lua
+++ base/nvim/init.lua
@@ -1,10 +1,12 @@
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.mouse = 'a'
-vim.o.wrap = true
+vim.o.wrap = false
+vim.o.tabstop = 2
 vim.o.breakindent = true
 vim.o.clipboard = "unnamedplus"
 vim.g.mapleader = " "
+vim.o.swapfile = false
 
 vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
 vim.keymap.set('n', '<leader>w', ':write<CR>')
@@ -15,11 +17,24 @@ vim.pack.add({
 'https://github.com/MeanderingProgrammer/render-markdown.nvim',
 'https://github.com/folke/which-key.nvim',
 'https://github.com/neovim/nvim-lspconfig',
+'https://github.com/nvim-mini/mini.pick'
 })
 
-vim.g.gruvbox_material_background = "medium"   -- or "soft", "hard"
-vim.g.gruvbox_material_palette    = "material" -- or "mix", "original"
-vim.cmd("colorscheme gruvbox-material")
+vim.api.nvim_create_autocmd('LspAttach', {
+    callback = function(ev)
+        local client = vim.lsp.get_client_by_id(ev.data.client_id)
+        if client.supports_method('textDocument/completion') then
+            vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+        end
+    end,
+})
+
+vim.cmd("set completeopt+=noselect")
+
+require 'mini.pick'.setup()
+vim.keymap.set('n', '<leader>f', ':Pick files<CR>')
+vim.keymap.set('n', '<leader>h', ':Pick help<CR>')
+vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
 
 vim.lsp.config('roslyn_ls', {
   cmd = {
@@ -33,6 +48,11 @@ vim.lsp.config('roslyn_ls', {
   },
 })
 
-vim.lsp.enable('roslyn_ls')
+vim.lsp.enable({"lua_ls", "roslyn_ls"})
 
 require('render-markdown').setup({})
+
+vim.g.gruvbox_material_background = "medium"   -- or "soft", "hard"
+vim.g.gruvbox_material_palette    = "material" -- or "mix", "original"
+vim.cmd("colorscheme gruvbox-material")
+
