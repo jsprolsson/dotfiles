5fb7de0d46790baab8ab18bbb29517db51ebd772
jsprolsson <jesper.p.olsson@hotmail.com>
2025-11-26 00:14:26 +0100

    structure in init.lua

--- base/nvim/init.lua
+++ base/nvim/init.lua
@@ -7,11 +7,18 @@ vim.o.breakindent = true
 vim.o.clipboard = "unnamedplus"
 vim.g.mapleader = " "
 vim.o.swapfile = false
+vim.o.scrolloff = 10
+vim.o.sidescrolloff = 8
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.incsearch = true
 
+-- basic keymaps
 vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
 vim.keymap.set('n', '<leader>w', ':write<CR>')
 vim.keymap.set('n', '<leader>q', ':quit<CR>')
 
+-- plugins installer
 vim.pack.add({
 	'https://github.com/sainnhe/gruvbox-material',
 	'https://github.com/MeanderingProgrammer/render-markdown.nvim',
@@ -22,19 +29,24 @@ vim.pack.add({
 	'https://github.com/neovim/nvim-lspconfig'
 })
 
+-- plugins activators
 require 'mini.pick'.setup()
-require 'oil'.setup()
+require 'oil'.setup({ view_options = { show_hidden = true } })
 require 'mason'.setup()
+require('lsp_configs.roslyn_ls')
+require('render-markdown').setup({})
+
+-- plugin keymaps
 vim.keymap.set('n', '<leader>f', ':Pick files<CR>')
 vim.keymap.set('n', '<leader>h', ':Pick help<CR>')
 vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
 vim.keymap.set('n', '<leader>rr', vim.lsp.buf.rename)
 vim.keymap.set('n', '<leader>e', ':Oil <CR>')
 
-vim.lsp.enable({ "lua_ls", "roslyn_ls" })
-require('lsp_configs.roslyn_ls')
-require('render-markdown').setup({})
 
+vim.lsp.enable({ "lua_ls", "roslyn_ls" })
+
+-- theme
 vim.g.gruvbox_material_background = "medium"
 vim.g.gruvbox_material_palette    = "material"
 vim.cmd("colorscheme gruvbox-material")
